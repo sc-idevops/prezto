c4c7e4efbd4b2694e361cf13fe772894712b91a6
steveokard <steveokard@gmail.com>
2022-07-23 01:57:59 -0400

    replaced vibinds with vi-mode from OMZ

--- contrib/vibinds/init.zsh
+++
@@ -1,9 +0,0 @@
-##Set VI MODE
-bindkey -v
-bindkey '^r' history-incremental-search-backward
-bindkey '^a' beginning-of-line
-bindkey '^e' end-of-line
-# Also fix annoying vi backspace
-bindkey '^?' backward-delete-char
-bindkey '^[b' vi-backward-blank-word
-bindkey '^[w' vi-forward-blank-word
