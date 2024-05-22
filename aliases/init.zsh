8a9843d94833264478609600d2aa226301387759
sc-idevops <stefenauris@proton.me>
2024-05-22 01:32:54 -0400

    added embed thumbnail

--- aliases/init.zsh
+++ aliases/init.zsh
@@ -51,4 +51,4 @@ fi
 alias f2b="sudo fail2ban-client status sshd"
 
 #yt-dlp
-alias marc='yt-dlp --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -x --audio-format mp3 --audio-quality 5'
+alias marc='yt-dlp --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -x --audio-format mp3 --audio-quality 5 --embed-thumbnail'
