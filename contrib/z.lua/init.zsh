579ac16d58a30b2734ab17bdcebd8271705754a2
steveokard <steveokard@gmail.com>
2024-08-24 18:19:15 -0400

    added aliases, remapped key to j

--- contrib/z.lua/init.zsh
+++ contrib/z.lua/init.zsh
@@ -1,1 +1,7 @@
+_ZL_CMD=j
+alias jj='j -c'      # restrict matches to subdirs of $PWD
+alias ji='j -i'      # cd with interactive selection
+alias jf='j -I'      # use fzf to select in multiple matches
+alias jb='j -b'      # quickly cd to the parent directory
+
 eval "$(lua ~/.zprezto/contrib/z.lua/external/z.lua --init zsh enhanced once echo fzf)"
